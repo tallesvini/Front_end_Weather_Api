65555744f048c56263d99ced97b7aed55bff678a
tallesvini <tallesvinicius62@gmail.com>
2023-05-28 11:40:13 -0300

    Adicao de geolocalizacao

--- js/script.js
+++ js/script.js
@@ -1,19 +1,69 @@
-$('#buscar').on('click', e => {
-  
-    const cidade = $('#cidade').val()
+const api = {
+    base_url: "https://api.openweathermap.org/data/2.5/",
+    lang: "pt_br",
+    units: "metric",
+    key: "3d4d8bf48b31fa547e29bb33ca994ff2"
+}
 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&units=metric&appid=3d4d8bf48b31fa547e29bb33ca994ff2&lang=pt_br`
+// geolocalização ..
+if('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        const url_1 = `${api.base_url}weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&lang=${api.lang}&units=${api.units}&APPID=${api.key}`
+
+        fetch(url_1).then(response => {
+            return response.json()
+        }).then((data) => {
+            $('#local').html(data.name)
+            $('#país').html(data.sys.country)
+            $('#temperatura').html(`${Math.round(data.main.temp)}°ᶜ`)
+
+            const iconName = data.weather[0].icon;
+            $('#imagem_temp').attr('src', `imagens/icons_temp/${iconName}.png`)
+            
+            const hm = data.weather[0].icon;
+            $('#slakk').attr('src', `imagens/wallpapers/${hm}.jpg`)
+          
+
+            $('#tempMax').html(`${Math.round(data.main.temp_max)}°C`)
+            $('#tempMin').html(`${Math.round(data.main.temp_min)}°C`)
+            $('#descricao').html(data.weather[0].description)
+            $('#humidade').html(`${Math.round(data.main.humidity)}%`)
+            $('#sensacao').html(`${Math.round(data.main.feels_like)}°C`)          
+        })
+    })
+} else {
+    alert('Opss... Localização não encontrada!')
+}
+
+
+// search ..
+$('#buscar').on('click', e => {
+
+    const cidade = $('#cidade').val()
+    const url_2 = `${api.base_url}weather?q=${cidade}&lang=${api.lang}&units=${api.units}&appid=${api.key}`
     
-    fetch(url).then(response => {
+    fetch(url_2).then(response => {
         return response.json()
-    }).then(data => {
+    }).then((data) => {
         $('#local').html(data.name)
+        $('#país').html(data.sys.country)
         $('#temperatura').html(`${Math.round(data.main.temp)}°ᶜ`)
+        
+        const iconName = data.weather[0].icon;
+        $('#imagem_temp').attr('src', `imagens/icons_temp/${iconName}.png`)
+        
+        const hm = data.weather[0].icon;
+        $('#slakk').attr('src', `imagens/wallpapers/${hm}.jpg`)
+          
+        
+          
         $('#tempMax').html(`${Math.round(data.main.temp_max)}°C`)
         $('#tempMin').html(`${Math.round(data.main.temp_min)}°C`)
         $('#descricao').html(data.weather[0].description)
         $('#humidade').html(`${Math.round(data.main.humidity)}%`)
         $('#sensacao').html(`${Math.round(data.main.feels_like)}°C`)
+    }).catch((error) => {
+        alert('Opss... Localização não encontrada!')
     })
-
 })
